56e8cb20adbab85786bbe6bcdf27568d08dafa40
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-06-11 08:57:19 -0700

    Bump tree-sitter to 0.21 (#3)
    * Bump tree-sitter to 0.21
    
    * Run tree-sitter generate
    
    * Rewrite Jest tests to use web-tree-sitter

--- test/crlf.test.js
+++ test/crlf.test.js
@@ -1,18 +1,39 @@
-const Parser = require('tree-sitter');
-const Talon = require('../bindings/node');
+const { beforeAll, describe, expect, it } = require('@jest/globals');
+const path = require('path');
+let ParserWasm = require('web-tree-sitter');
+let ParserNode = require('tree-sitter');
 
-const parser = new Parser();
-parser.setLanguage(Talon);
-
-describe('Parser', function () {
-    it('should strip newlines from comments', function () {
-        const sourceCode = '# Comment\n-';
-        const tree = parser.parse(sourceCode);
-        expect(tree.rootNode.child(0).text).toEqual('# Comment');
-    });
-    it('should strip CRLFs from comments', function () {
-        const sourceCode = '# Comment\r\n-';
-        const tree = parser.parse(sourceCode);
-        expect(tree.rootNode.child(0).text).toEqual('# Comment');
-    });
+describe.each([
+  { name: 'wasm' },
+  { name: 'node' },
+])('tree-sitter-talon [$name]', ({ name }) => {
+  let parser;
+  beforeAll(async () => {
+    // Initialise web-tree-sitter
+    return await ParserWasm.init();
+  });
+  beforeEach(async () => {
+    // Load tree-sitter-talon.wasm
+    if (name === 'wasm') {
+      const pathToTreeSitterTalonWasm = path.resolve(__dirname, '..', 'tree-sitter-talon.wasm')
+      const TalonWasm = await ParserWasm.Language.load(pathToTreeSitterTalonWasm);
+      parser = new ParserWasm().setLanguage(TalonWasm);
+    }
+    // Load tree-sitter-talon.node
+    if (name === 'node') {
+      const pathToTreeSitterTalonNode = `../prebuilds/${process.platform}-${process.arch}/tree-sitter-talon.node`
+      const TalonNode = require(pathToTreeSitterTalonNode);
+      parser = new ParserNode().setLanguage(TalonNode);
+    }
+  });
+  it('should strip newlines from comments', () => {
+    const sourceCode = '# Comment\n-';
+    const tree = parser.parse(sourceCode);
+    expect(tree.rootNode.child(0).text).toEqual('# Comment');
+  })
+  it('should strip CRLFs from comments', () => {
+    const sourceCode = '# Comment\r\n-';
+    const tree = parser.parse(sourceCode);
+    expect(tree.rootNode.child(0).text).toEqual('# Comment');
+  });
 });
