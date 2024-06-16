a358114cc9c78bbddbe2950efc204afdeba6c730
Wen Kokke <wenkokke@users.noreply.github.com>
2024-06-16 16:06:15 +0100

    Split node and wasm tests

--- test/crlf.test.js
+++ test/crlf.test.js
@@ -1,39 +1,39 @@
 const { beforeAll, describe, expect, it } = require('@jest/globals');
 const path = require('path');
-let ParserWasm = require('web-tree-sitter');
-let ParserNode = require('tree-sitter');
+const ParserNode = require('tree-sitter');
+const ParserWasm = require('web-tree-sitter');
 
-describe.each([
-  { name: 'wasm' },
-  { name: 'node' },
-])('tree-sitter-talon [$name]', ({ name }) => {
-  let parser;
-  beforeAll(async () => {
-    // Initialise web-tree-sitter
-    return await ParserWasm.init();
+const MODES = ['node', 'wasm'];
+describe.each(
+  MODES.includes(process.env.MODE) ? [process.env.MODE] : MODES
+)('tree-sitter-talon [%s]',
+  (MODE) => {
+    let parser;
+    beforeEach(async () => {
+      switch (MODE) {
+        // Load tree-sitter and tree-sitter-talon.node
+        case 'node': {
+          const pathToTreeSitterTalonNode = `../prebuilds/${process.platform}-${process.arch}/tree-sitter-talon.node`
+          const TalonNode = require(pathToTreeSitterTalonNode);
+          parser = new ParserNode().setLanguage(TalonNode);
+        };
+        // Load web-tree-sitter and tree-sitter-talon.wasm
+        case 'wasm': {
+          await ParserWasm.init();
+          const pathToTreeSitterTalonWasm = path.resolve(__dirname, '..', 'tree-sitter-talon.wasm')
+          const TalonWasm = await ParserWasm.Language.load(pathToTreeSitterTalonWasm);
+          parser = new ParserWasm().setLanguage(TalonWasm);
+        };
+      }
+    });
+    it('should strip newlines from comments', () => {
+      const sourceCode = '# Comment\n-';
+      const tree = parser.parse(sourceCode);
+      expect(tree.rootNode.child(0).text).toEqual('# Comment');
+    })
+    it('should strip CRLFs from comments', () => {
+      const sourceCode = '# Comment\r\n-';
+      const tree = parser.parse(sourceCode);
+      expect(tree.rootNode.child(0).text).toEqual('# Comment');
+    });
   });
-  beforeEach(async () => {
-    // Load tree-sitter-talon.wasm
-    if (name === 'wasm') {
-      const pathToTreeSitterTalonWasm = path.resolve(__dirname, '..', 'tree-sitter-talon.wasm')
-      const TalonWasm = await ParserWasm.Language.load(pathToTreeSitterTalonWasm);
-      parser = new ParserWasm().setLanguage(TalonWasm);
-    }
-    // Load tree-sitter-talon.node
-    if (name === 'node') {
-      const pathToTreeSitterTalonNode = `../prebuilds/${process.platform}-${process.arch}/tree-sitter-talon.node`
-      const TalonNode = require(pathToTreeSitterTalonNode);
-      parser = new ParserNode().setLanguage(TalonNode);
-    }
-  });
-  it('should strip newlines from comments', () => {
-    const sourceCode = '# Comment\n-';
-    const tree = parser.parse(sourceCode);
-    expect(tree.rootNode.child(0).text).toEqual('# Comment');
-  })
-  it('should strip CRLFs from comments', () => {
-    const sourceCode = '# Comment\r\n-';
-    const tree = parser.parse(sourceCode);
-    expect(tree.rootNode.child(0).text).toEqual('# Comment');
-  });
-});
